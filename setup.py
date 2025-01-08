81aa6a66030bbb19a2d7c74bf382c8c1781caf1c
marco <marco@sizewise.app>
2025-01-08 18:45:36 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 # YOLO v9, GPL-3.0 license
 
 __version__ = "0.3.01"
-
-import os
+# import os
 from setuptools import setup, find_packages
 
 # Set ENV variables - place before imports -
-if not os.environ.get("OMP_NUM_THREADS"):
-    os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
+# if not os.environ.get("OMP_NUM_THREADS"):
+#     os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
 
 # Define the setup configuration
 setup(
     name="yolov9",  # Package name
     version=__version__,
-    author="Your Name or Organization",
+    author="Sizewise",
     author_email="your_email@example.com",
     description="YOLOv9: A new version of YOLO object detection framework",
     long_description=open("README.md").read(),  # Include README content
@@ -22,7 +21,7 @@ setup(
     license="GPL-3.0",
     packages=find_packages(),  # Automatically find packages (like `models`)
     include_package_data=True,  # Include non-Python files specified in MANIFEST.in
-    python_requires=">=3.7",  # Specify Python version compatibility
+    python_requires=">=3.9",  # Specify Python version compatibility
     install_requires=open("requirements.txt").read().splitlines(),  # Dependencies from requirements.txt
     classifiers=[
         "Programming Language :: Python :: 3",
--- yolov9/__init__.py
+++ yolov9/__init__.py
@@ -1,9 +1,9 @@
 #  YOLO v9, GPL-3.0 license
-
-__version__ = "0.3.00"
-
-import os
-
-# Set ENV variables -place before imports-
-if not os.environ.get("OMP_NUM_THREADS"):
-    os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
+#
+# __version__ = "0.3.00"
+#
+# import os
+#
+# # Set ENV variables -place before imports-
+# if not os.environ.get("OMP_NUM_THREADS"):
+#     os.environ["OMP_NUM_THREADS"] = "1"  # default for reduced CPU utilization during training
